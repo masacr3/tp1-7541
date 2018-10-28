2a1d54fea01a414c56eff3ce5a024be3a3e7a6b4
Leonel R <leorhcp2030@gmail.com>
2018-10-28 11:36:43 -0300

    se agregaron las estructuras de control de errores

--- greep.c
+++ greep.c
@@ -1,73 +1,72 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
-bool buscar(char *exp, char *cadena, int *i);
-void lookposition(int p,char *e, char *c);
-void msj(char *s);
+bool buscar(char *exp, char *cadena);
 
-int main(){
-	char *e = "od";
-	char *e2 = "o r";
-	char *cad = "hola como estas todo bien leo";
-	char *cad2 = "la verdad la otra vez el gordo rata me choreo la garrafa";
-	int j = 0;
-	int i = 0;
-	msj("¬¬ test ¬¬\n");
-
-	if ( buscar ( e, cad, &i) ) lookposition(i,e,cad);
-	else printf("\n\nno se encontro nada\n\n");
-
-	if ( buscar( e2, cad2, &j) ) lookposition(j,e2,cad2);
-	else printf("\n\nno se encontro nada\n\n");
+int main(int argc, char *argv[]){
+	if ( argc < 3 ){ 
+		fprintf(stderr,"%s","Cantidad de parametros erronea\n");
+		return 0;
+	}
 	
-	j=0;
+	int numero = atoi(argv[2]);
 
-	if ( buscar( e, cad2, &j) ) lookposition(j,e,cad2);
-	else printf("\n\nno se encontro nada\n\n");	
+	if( (!numero && strcmp(argv[2],"0") != 0)  || numero < 0) {
+		fprintf(stderr,"%s","Cantidad de parametros erronea\n");	
+		return 0;
+	}
+
+	if ( argc == 4 ){
+		FILE *f;
+		if ((f = fopen(argv[3],"r")) == NULL ){
+			fprintf(stderr,"%s\n","No se pudo leer el archivo indicado");
+			return 0;
+		}
+		
+		char *linea = NULL;
+		size_t cap = 0;
+		while(true){
+			if ( getline(&linea,&cap,f) == EOF ) break;
+			
+			if ( buscar(argv[1],linea) ) printf("%s\n",linea);
+		}
+		fclose(f);
+		free(linea);		
+	}
+	else{
 	
+	}
+
 	return 0;
 }
 
-void msj(char *s){
-	printf("%s\n",s);
-}
-
-bool buscar(char *exp, char *cadena, int *i){
+bool buscar(char *exp, char *cadena){
 	size_t le = strlen(exp);
 	size_t lc = strlen(cadena);
 
 	if ( le > lc ) return false;
 	
 	int match;
+	int i = 0;
 
-	while( *i < lc - le ){
-		if ( cadena[*i] == exp[0] ) {
+	while( i < lc - le ){
+		if ( cadena[i] == exp[0] ) {
 			int j = 0;
 			match = 0;
-			while( j < le && *i < lc ){
-				if ( cadena[*i] == exp[j] ){
+			while( j < le && i < lc ){
+				if ( cadena[i] == exp[j] ){
 					match++;
-					*i += 1;
+					i += 1;
 					j++;
 					if ( match == le ) return true;
 				}
 				else break;
 			}
 		}
-		*i += 1 ;
+		i += 1 ;
 	}
 	return false;
-}
-
-void lookposition(int p,char *e, char *c){
-	printf("texto->");
-	msj(c);
-	printf("exp--->");
-	for (int i=0; i<p-strlen(e); i++){
-		printf(" ");
-	}
-	msj(e);
-	printf("\n");
 }
