d18310c8ca7a4457a7044f3539e88037460307e5
Leonel R <leorhcp2030@gmail.com>
2018-10-29 21:53:28 -0300

    se agrego las lecturas por stdin y por archivo
    se modifico la estructura interna del main. el contenido de las operaciones se encapsulo en la funcion grep

--- greep.c
+++ greep.c
@@ -3,47 +3,104 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include "cola.h"
 
-bool buscar(char *exp, char *cadena);
+bool buscar_expresion(char *exp, char *cadena);
+void mostrar_lineas(cola_t *);
+void grep(int argc, char *argv[]);
 
 int main(int argc, char *argv[]){
+	grep(argc, argv);
+	return 0;
+}
+
+void grep(int argc, char *argv[]){
 	if ( argc < 3 ){ 
 		fprintf(stderr,"%s","Cantidad de parametros erronea\n");
-		return 0;
+		return;
 	}
 	
-	int numero = atoi(argv[2]);
+	int n = atoi(argv[2]);
 
-	if( (!numero && strcmp(argv[2],"0") != 0)  || numero < 0) {
+	if( (!n && strcmp(argv[2],"0") != 0)  || n < 0) {
 		fprintf(stderr,"%s","Cantidad de parametros erronea\n");	
-		return 0;
+		return;
 	}
+	
+	cola_t *cola = cola_crear();
 
 	if ( argc == 4 ){
 		FILE *f;
-		if ((f = fopen(argv[3],"r")) == NULL ){
+		char *archivo = argv[3];
+		if ((f = fopen(archivo,"r")) == NULL ){
 			fprintf(stderr,"%s\n","No se pudo leer el archivo indicado");
-			return 0;
+			cola_destruir(cola,NULL);
+			return;
 		}
 		
 		char *linea = NULL;
 		size_t cap = 0;
+		int i = 0;
+		char *expresion = argv[1];
+			
 		while(true){
 			if ( getline(&linea,&cap,f) == EOF ) break;
 			
-			if ( buscar(argv[1],linea) ) printf("%s\n",linea);
+			char *nueva_linea = malloc(sizeof(char) * ( strlen(linea) + 1) );
+			strcpy(nueva_linea,linea);
+			cola_encolar(cola,nueva_linea);
+
+			if ( i > n ){
+				char *borrar_linea = cola_desencolar(cola);
+				free(borrar_linea);
+			}
+			
+			i++;
+
+			if ( buscar_expresion(expresion, linea) ) {
+				mostrar_lineas(cola);
+				i = 0;
+			}
+			
+			
 		}
+		cola_destruir(cola,free);
 		fclose(f);
 		free(linea);		
 	}
 	else{
-	
+		char *linea = NULL;
+		size_t cap = 0;
+		int i = 0;
+		char *expresion = argv[1];
+		
+		while(true){
+			if ( getline(&linea,&cap,stdin) == EOF ) break;
+
+			char*nueva_linea = malloc( sizeof(char) * ( strlen(linea) + 1) );
+			strcpy(nueva_linea,linea);
+			cola_encolar(cola, nueva_linea);
+
+			if ( i > n ) {
+				char *borrar_linea = cola_desencolar(cola);
+				free(borrar_linea);
+			}
+			
+			i++;
+
+			if ( buscar_expresion(expresion, linea)) {
+				mostrar_lineas(cola);
+				i = 0;
+			}
+		}
+		cola_destruir(cola,free);
+		free(linea);
 	}
 
-	return 0;
+	return;
 }
 
-bool buscar(char *exp, char *cadena){
+bool buscar_expresion(char *exp, char *cadena){
 	size_t le = strlen(exp);
 	size_t lc = strlen(cadena);
 
@@ -69,4 +126,13 @@ bool buscar(char *exp, char *cadena){
 		i += 1 ;
 	}
 	return false;
+}
+
+void mostrar_lineas(cola_t *cola){
+	while (!cola_esta_vacia(cola)){
+		char *linea = cola_desencolar(cola);
+		fprintf(stdout,"%s",linea);
+		free(linea);
+	}
+
 }
